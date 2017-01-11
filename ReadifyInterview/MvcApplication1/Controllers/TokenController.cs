663936421673d3ef57a3d96068f4b570c89e93cd
savithri-uvr <savithriv.net@gmail.com>
2017-01-11 20:53:06 +1100

    Add files via upload

---
+++ ReadifyInterview/MvcApplication1/Controllers/TokenController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class TokenController : ApiController
+    {
+        // GET api/token
+        public string Get()
+        {
+            return "824ba76e-7f33-4af3-ab67-06ba9679fb36";
+        }
+
+        // GET api/token/5
+        public string WhatIsYourToken(string strInputValue)
+        {
+            return strInputValue;
+        }
+    }
+}
